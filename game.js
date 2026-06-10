d92c8599d16e51c2c5286864374af360e74df83d
JANENDRA SINGH <singhjanendra120202@gmail.com>
2026-06-10 15:50:29 +0530

    Start the Game

--- game.js
+++ game.js
@@ -3,7 +3,27 @@ var gamePattern = [];
 var userClickedPattern = []; 
 var randomChosenColour ;
 
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+    if(!started){
+        $("#level-title").html("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
 function nextSequence(){
+    level++;
+    $("#level-title").html("Level " + level); 
     var randomNumber = Math.floor(Math.random() * 4);
     randomChosenColour=buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -11,23 +31,13 @@ function nextSequence(){
     playSound(randomChosenColour);
 } 
 
-$(".btn").click(function handel() {
-    var userChosenColour = $(this).attr("id");
-    userClickedPattern.push(userChosenColour);
-    playSound(userChosenColour);
-    console.log(userClickedPattern);
-    animatePress(userChosenColour);
-});
-
 function playSound(name){
     var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();   
 }
 
 function animatePress(currentColour){
-
     $("#" + currentColour).addClass("pressed");
-
     setTimeout(function(){
         $("#" + currentColour).removeClass("pressed");
     }, 100);
